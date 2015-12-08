921fc57c6a38e581fd747829b96ec2367f82e8f9
jsdev <per-erik.karlsson@tieto.com>
2015-12-08 11:40:42 +0200

    Safe space

--- src/client.js
+++ src/client.js
@@ -1,3 +1,25 @@
 import './client.less';
 
-console.log('quack says the duck');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import store from './store';
+
+import { Provider } from 'react-redux';
+
+import TodoApp from './components/smart/TodoAppContainer.js';
+
+
+// import todos from './todos'
+// console.log('todos', todos.toJS());
+
+const app = (
+    <Provider store={store}>
+        <TodoApp />
+    </Provider>
+)
+
+
+ReactDOM.render(
+    app,
+    document.getElementById('app')
+)
